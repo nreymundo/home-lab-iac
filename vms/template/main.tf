cde797e5689c7db3670e2da2e3bc796369c3e319
Nicolas Reymundo <nreymundo@users.noreply.github.com>
2022-04-16 23:24:46 +0200

    Added config files to create 2 types of VMs

---
+++ vms/template/main.tf
@@ -0,0 +1,11 @@
+module "vm" {
+  source            = "../../terraform/modules/proxmox-vm"
+
+  vm_name           = "template"
+  vm_cpu_type       = "host"
+  proxmox_host      = var.proxmox_host
+  ansible_playbook  = "./ansible-bootstrap.yml"
+}
+
+variable "proxmox_host" {
+}
