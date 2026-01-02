a93d92fc6baa813e44edc7d217ebadfccbe9efaf
Nicolas Reymundo <nreymundo@users.noreply.github.com>
2026-01-02 18:17:24 +0100

    refactor(packer): simplify Bitwarden secret setup

--- packer/scripts/bws-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Bitwarden Secret IDs for Packer
-# Safe to commit - useless without BWS_ACCESS_TOKEN
-
-export BWS_ORG_ID="377d85e1-fff0-41de-a1e4-b3bc01566f70"
-export BWS_SSH_KEYS_ID="9b5f1231-f792-4e85-96f1-b3c60002f839"
