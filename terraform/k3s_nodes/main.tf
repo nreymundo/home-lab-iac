8157cf41b14bfc26e5ebcdce75bc834bb70d0fd9
Nicolas Reymundo <nreymundo@users.noreply.github.com>
2026-01-02 19:29:10 +0100

    Allow different base OSs per VM (#81)

--- terraform/k3s_nodes/main.tf
+++ terraform/k3s_nodes/main.tf
@@ -1,6 +1,13 @@
 locals {
   node_ip_subnet      = "${join(".", slice(split(".", var.node_ip_start), 0, 3))}.0/${var.ip_prefix_len}"
   node_ip_start_octet = tonumber(split(".", var.node_ip_start)[3])
+  node_count          = length(var.nodes)
+  node_ci_users = [
+    for idx in range(local.node_count) : coalesce(try(var.nodes[idx].ci_user, null), var.default_ci_user)
+  ]
+  node_ansible_users = [
+    for idx in range(local.node_count) : coalesce(try(var.nodes[idx].ansible_user, null), local.node_ci_users[idx])
+  ]
   ssh_public_keys_list = compact(
     split(
       "\n",
@@ -10,13 +17,13 @@ locals {
 }
 
 resource "proxmox_vm_qemu" "k3s_nodes" {
-  count = var.node_count
+  count = local.node_count
 
   name        = "k3s-node-${format("%02d", count.index + 1)}"
-  target_node = element(var.proxmox_nodes, count.index % length(var.proxmox_nodes))
+  target_node = coalesce(try(var.nodes[count.index].target_node, null), element(var.proxmox_nodes, count.index % length(var.proxmox_nodes)))
   vmid        = var.node_vmid_start + count.index
 
-  clone      = var.template_name
+  clone      = coalesce(try(var.nodes[count.index].template_name, null), var.template_name)
   full_clone = true
 
   scsihw = "virtio-scsi-single"
@@ -30,7 +37,7 @@ resource "proxmox_vm_qemu" "k3s_nodes" {
   memory = var.vm_memory_mb
 
   os_type = "cloud-init"
-  ciuser  = "ubuntu"
+  ciuser  = local.node_ci_users[count.index]
   sshkeys = join("\n", local.ssh_public_keys_list)
 
 
@@ -100,9 +107,11 @@ resource "local_file" "ansible_inventory" {
 
   content = templatefile("${path.module}/templates/inventory.yaml.tpl", {
     nodes = [
-      for idx in range(var.node_count) : {
-        name = "k3s-node-${format("%02d", idx + 1)}"
-        ip   = cidrhost(local.node_ip_subnet, local.node_ip_start_octet + idx)
+      for idx in range(local.node_count) : {
+        name         = "k3s-node-${format("%02d", idx + 1)}"
+        ip           = cidrhost(local.node_ip_subnet, local.node_ip_start_octet + idx)
+        ansible_user = local.node_ansible_users[idx]
+        node_os      = local.node_ci_users[idx]
       }
     ]
   })
--- terraform/k3s_nodes/templates/inventory.yaml.tpl
+++ terraform/k3s_nodes/templates/inventory.yaml.tpl
@@ -11,6 +11,7 @@ all:
 %{ for node in nodes ~}
         ${node.name}:
           ansible_host: ${node.ip}
-          ansible_user: "ubuntu"
+          ansible_user: "${node.ansible_user}"
           ansible_port: 22
+          node_os: "${node.node_os}"
 %{ endfor ~}
